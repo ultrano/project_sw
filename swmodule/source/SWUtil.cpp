75496f08c90b0bcfef468ca5752a8eda943d2f99
storypalse <storypalse@gmail.com>
2014-01-27 13:58:01 +0900

    applying prototype memory core

--- swmodule/source/SWUtil.cpp
+++ swmodule/source/SWUtil.cpp
@@ -11,6 +11,185 @@
 #include <sys/time.h>
 #endif
 
+#ifdef _MSC_VER
+#pragma pack(push,1)
+#endif
+struct MemBlock
+{
+	MemBlock* next;
+	bool isUsing;
+
+	bool isTail()
+	{
+		return ( next == NULL );
+	};
+
+	unsigned int capacity()
+	{
+		if ( isTail() ) return 0;
+		unsigned int p = (unsigned int)((char*)next - ((char*)this + sizeof(MemBlock)));
+		return p;
+	};
+
+	void* memory()
+	{
+		return (void*)((char*)this + (sizeof(MemBlock)/sizeof(char)));
+	}
+
+	bool merge()
+	{
+		MemBlock* old = next;
+		while( next != NULL )
+		{
+			if ( next->isTail() ) break;
+			if ( next->isUsing ) break;
+			next = next->next;
+		}
+		return ( old != next );
+	}
+
+	bool divide( unsigned int size )
+	{
+		if ( capacity() < (size + sizeof(MemBlock)) ) return false;
+		
+		MemBlock* sep = (MemBlock*)((char*)(memory()) + size);
+		sep->isUsing = false;
+		sep->next = next;
+		next = sep;
+		return true;
+	}
+};
+
+struct MemRod
+{
+	MemRod* next;
+	unsigned int maximumCapacity;
+	MemBlock     original;
+	
+	static MemRod* createRod( unsigned int size )
+	{
+		if ( size == 0 ) return NULL;
+		unsigned int baseSize = (sizeof(MemRod) + sizeof(MemBlock));
+		unsigned int totalSize = baseSize + size;
+		void* chunk = malloc( totalSize );
+		
+		MemRod*   rod  = (MemRod*)chunk;
+		MemBlock* head = (MemBlock*)&rod->original;
+		MemBlock* tail = (MemBlock*)((char*)(chunk) + totalSize - sizeof(MemBlock));
+		
+		rod->next = NULL;
+
+		head->next = tail;
+		tail->next = NULL;
+
+		head->isUsing = false;
+		tail->isUsing = true;
+
+		rod->maximumCapacity = head->capacity();
+
+		return rod;
+	}
+
+	static void deleteRod( MemRod* rod )
+	{
+		::free( (void*)rod );
+	}
+
+	MemBlock* request( unsigned int size )
+	{
+		MemBlock* block = &original;
+		while( block != NULL )
+		{
+			if ( block->isTail() ) return NULL;
+			if ( false == block->isUsing )
+			{
+				if ( size <= block->capacity() ) break;
+				 block->merge();
+				if ( size <= block->capacity() ) break;
+			}
+			block = block->next;
+		}
+		
+		if ( block->isTail() ) return NULL;
+
+		block->divide( size );
+
+		return block;
+	}
+
+	void defragment()
+	{
+		MemBlock* block = &original;
+		while( block != NULL )
+		{
+			if ( block->isTail() ) return ;
+			block->merge();
+			block = block->next;
+		}
+	}
+};
+#ifdef _MSC_VER
+#pragma pack(pop)
+#endif
+
+class MemCore
+{
+	enum { DEFAULT_MAX_CAPACITY = 512 };
+	MemRod* m_fuelRod;
+
+	MemCore()
+		: m_fuelRod( MemRod::createRod( DEFAULT_MAX_CAPACITY ) )
+	{
+	};
+	~MemCore()
+	{
+	}
+
+public:
+
+	static MemCore& instance()
+	{
+		static MemCore core;
+		return core;
+	}
+
+	void* alloc( unsigned int size )
+	{
+		MemRod* rod = m_fuelRod;
+		MemBlock* block = NULL;
+
+		do
+		{
+			block = rod->request( size );
+			if ( block != NULL ) break;
+			if ( rod->next == NULL ) rod->next = MemRod::createRod( DEFAULT_MAX_CAPACITY );
+			rod = rod->next;
+		} while ( true );
+
+		if ( block == NULL ) return NULL;
+		block->divide( size );
+		block->isUsing = true;
+		return block->memory();
+	}
+
+	void free( void* mem )
+	{
+		MemBlock* block = (MemBlock*)((char*)mem - (sizeof(MemBlock)/sizeof(char)));
+		block->isUsing = false;
+		block->merge();
+	}
+	
+	void defragment()
+	{
+		MemRod* rod = m_fuelRod;
+		while ( rod != NULL )
+		{
+			rod->defragment();
+			rod = rod->next;
+		}
+	}
+};
+
 __SWUtil::__SWUtil()
 {
 }
@@ -40,12 +219,12 @@ unsigned int __SWUtil::getMicroCount()
 
 void* __SWUtil::alloc( size_t size )
 {
-	return malloc( size );
+	return MemCore::instance().alloc( size );
 }
 
 void  __SWUtil::free( void* memory )
 {
-	::free( memory );
+	MemCore::instance().free( memory );
 }
 
 tnumber __SWUtil::strToNum( const tstring& str )
