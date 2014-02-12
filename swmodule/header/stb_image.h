2b1fdf7885f3bf700667cde06a4dd6b39611f716
storypalse <storypalse@gmail.com>
2014-02-12 17:17:46 +0900

    gcc compile fix

--- swmodule/header/stb_image.h
+++ swmodule/header/stb_image.h
@@ -1,4 +1,4 @@
-#define STBI_HEADER_FILE_ONLY
+//#define STBI_HEADER_FILE_ONLY
 /* stbi-1.33 - public domain JPEG/PNG reader - http://nothings.org/stb_image.c
    when you control the images you're loading
                                      no warranty implied; use at your own risk
