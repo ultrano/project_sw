9bd2442fc9a5b6b0a250981571ea3a137fc087cb
storypalse <storypalse@gmail.com>
2014-02-13 16:44:44 +0900

    compile bat commit

--- swmodule/compile.bat
+++ swmodule/compile.bat
@@ -1,3 +1,3 @@
 g++ -c -I./header -I../opengl ./source/*.cpp ./source/*.c
-g++ -o libmodule.so -shared *.o -l../opengl/glew32.dll
+g++ -o libmodule.so -shared *.o -L../opengl/ -lglew32 -lopengl32 -lglut32 -lglut -lglu32 -lws2_32
 pause
