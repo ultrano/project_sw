f49ee57f810d577cc373525999172d836d3c4e93
storypalse <storypalse@gmail.com>
2014-02-14 16:01:19 +0900

    apply osx, ios compile

--- swmodule/source/SWGameContext.cpp
+++ swmodule/source/SWGameContext.cpp
@@ -2,12 +2,20 @@
 #include "SWGameContext.h"
 
 #ifdef WIN32
-#include "glew.h"
-#else
-#include <OpenGLES/ES1/gl.h>
-#include <OpenGLES/ES1/glext.h>
-#include <OpenGLES/ES2/gl.h>
-#include <OpenGLES/ES2/glext.h>
+  #include "glew.h"
+#elif __APPLE__
+  #include "TargetConditionals.h"
+  #ifdef TARGET_OS_MAC
+    #include <OpenGL/gl.h>
+    #include <OpenGL/glu.h>
+    #include <OpenGL/glext.h>
+    #include <GLUT/glut.h>
+  #elif TARGET_OS_IPHONE
+    #include <OpenGLES/ES1/gl.h>
+    #include <OpenGLES/ES1/glext.h>
+    #include <OpenGLES/ES2/gl.h>
+    #include <OpenGLES/ES2/glext.h>
+  #endif
 #endif
 
 #include <memory>
