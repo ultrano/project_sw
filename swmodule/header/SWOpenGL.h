5dd333330eb80e32251736c35d8898d75343eb17
hahasasa <storypalse@gmail.com>
2014-04-15 23:50:08 +0900

    OpenGL include 분기 추가.

--- swmodule/header/SWOpenGL.h
+++ swmodule/header/SWOpenGL.h
@@ -1,21 +1,23 @@
 #ifndef SWOpenGL_h__
 #define SWOpenGL_h__
 
-#ifdef WIN32
+#ifdef WINDOWS
 # include "glew.h"
-#elif __APPLE__
-#  include "TargetConditionals.h"
-# ifdef TARGET_OS_IPHONE
-#   include <OpenGLES/ES1/gl.h>
-#   include <OpenGLES/ES1/glext.h>
-#   include <OpenGLES/ES2/gl.h>
-#   include <OpenGLES/ES2/glext.h>
-#  elif TARGET_OS_MAC
-#   include <OpenGL/gl.h>
-#   include <OpenGL/glu.h>
-#   include <OpenGL/glext.h>
-#   include <GLUT/glut.h>
-#  endif
+#elif IOS
+# include <OpenGLES/ES1/gl.h>
+# include <OpenGLES/ES1/glext.h>
+# include <OpenGLES/ES2/gl.h>
+# include <OpenGLES/ES2/glext.h>
+#elif OSX
+# include <OpenGL/gl.h>
+# include <OpenGL/glu.h>
+# include <OpenGL/glext.h>
+# include <GLUT/glut.h>
+#elif ANDROID
+# include <jni.h>
+# include <GLES2/gl2.h>
+# include <GLES2/gl2ext.h>
 #endif
+
 
 #endif // SWOpenGL_h__
